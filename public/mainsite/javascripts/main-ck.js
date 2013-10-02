da75dd907a764e23eecb4f0c52ed9d7804a51af3
taylorkmho <taylorkmho@gmail.com>
2013-10-02 10:15:12 -1000

    Changes per Malia feedback

--- public/mainsite/javascripts/main-ck.js
+++ public/mainsite/javascripts/main-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$(".criteriatitle").on("mouseover",function(){$("a.criteriapdf").fadeIn("fast").css("display","block")}).on("mouseout",function(){$("a.criteriapdf").css("display","none")})});
--- public/mainsite/javascripts/main.js
+++ public/mainsite/javascripts/main.js
@@ -1,3 +1,7 @@
 $(document).ready( function(){
-
+	$('.criteriatitle').on('mouseover', function (){
+		$('a.criteriapdf').fadeIn('fast').css('display','block');
+	}).on('mouseout',function(){
+		$('a.criteriapdf').css('display','none');
+	});
 });
