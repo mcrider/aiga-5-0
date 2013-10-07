0027c99908295738eb1049aadf6ccbc86a615852
Matt Crider <mattcrider@gmail.com>
2013-10-06 21:12:36 -1000

    Download CSV of user data

--- client/views/admin/admin_users.js
+++ client/views/admin/admin_users.js
@@ -37,9 +37,70 @@ Template.admin_users.events = {
     Meteor.users.remove(userId);
   },
   'click .numberCircle': function(e) {
+    e.preventDefault();
     Session.set('adminUserId', this._id);
+  },
+  'click .get-user-data': function(e) {
+    data = [["ID","Type","Submitter's Name","Submitter's Email","Submitter's AIGA ID","Project Name","Client Name","Date paid","URL/Paper Company","Art Director","Designer","Illustrator","Copywriter","Photographer","Special Consultant","Paper","Developer","Animator","Technical information","Project Description","Notes"]];
+    var blankOrVal = function(val) {
+      if(val) return val;
+      else return "";
+    }
+    Entries.find().forEach(function(item) {
+      var user = Meteor.users.findOne(item.userId);
+      entry = [];
+      entry.push(blankOrVal(item._id)); // ID
+      entry.push(blankOrVal(item.type)); // Type
+
+      if(user) {
+        entry.push(blankOrVal(user.profile.contact_name)); // Submitter's Name
+        entry.push(blankOrVal(user.emails[0].address)); // Submitter's Email
+        entry.push(blankOrVal(user.profile.aiga_id)); // Submitter's AIGA ID
+      } else entry.concat(["","",""]);
+      entry.push(blankOrVal(item.project_name)); // Project Name
+      entry.push(blankOrVal(item.client_name)); // Client Name
+      if(!item.paid) {
+        entry.push("");
+      } else {
+        entry.push(utils.displayHumanReadableDate(item.paid)); // Date paid
+      }
+      entry.push(blankOrVal(item.url)); // URL/Paper Company
+
+      entry.push(blankOrVal(item.art_director)); // Art Director
+      entry.push(blankOrVal(item.designer)); // Designer
+      entry.push(blankOrVal(item.illustrator)); // Illustrator
+      entry.push(blankOrVal(item.copywriter)); // Copywriter
+      entry.push(blankOrVal(item.photographer)); // Photographer
+      entry.push(blankOrVal(item.special_consultant)); // Special Consultant
+      entry.push(blankOrVal(item.paper)); // Paper
+      entry.push(blankOrVal(item.developer)); // Developer
+      entry.push(blankOrVal(item.animator)); // Animator
+      entry.push(blankOrVal(item.tech_info)); // Technical information
+      entry.push(blankOrVal(item.project_description)); // Project Description
+      entry.push(blankOrVal(item.notes)); // Notes
+
+
+      data.push(entry);
+    })
+    // var data = [["name1", "city1", "some other info"], ["name2", "city2", "more info"]];
+    var csvContent = "data:text/csv;charset=utf-8,";
+    data.forEach(function(infoArray, index){
+       dataString = infoArray.join(",");
+       csvContent += index < infoArray.length ? dataString+ "\n" : dataString;
+    });
+
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
+
+    // var encodedUri = encodeURI(csvContent);
+    // var link = document.createElement("a");
+    // link.setAttribute("href", encodedUri);
+    // link.setAttribute("download", "my_data.csv");
+
+    // link.click();
   }
 };
+
 
 // Renders the header template (delaying load until site settings are available)
 Template.admin_users.helpers({
